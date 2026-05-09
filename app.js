a77ea9b6f819a504817c363fd44592afdd8f1d79
Tauqeer Ahmed baig <tauqeerahmedbaig@gmail.com>
2026-05-10 00:53:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// ✅ Legal variable names
+var firstName = "Shahzad";
+var last_name = "Ahmed";
+var user1 = "User";
+var $price = 100;
+var _total = 500;
+
+console.log(firstName, last_name, user1, $price, _total);
+
+// ✅ Camel Case examples
+var myFirstName = "Shahzad";
+var totalMarksObtained = 450;
+
+console.log(myFirstName, totalMarksObtained);
